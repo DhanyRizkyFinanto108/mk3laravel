909b6f0a61576159b2d615db90df8532fc0065ad
DhanyRizkyFinanto108 <541231048@student.smktelkom-pwt.sch.id>
2024-10-16 14:33:28 +0700

    add index and login page

---
+++ resources/views/index.blade.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<link type="icon" rel="icon" href="img/icon/favicon.ico" />
+	<title>Dhani.</title>
+	<link rel="stylesheet" href="assets/css/style.css" />
+</head>
+<body>
+	<header>
+		<a class="brand">Website</a>
+		<div></div>
+		<div class="navigasi" style="font-weight: bold">
+			<strong>
+				<a href="#Home">Home</a>
+				<a href="#About">About</a>
+				<a href="#Hoby">Hoby</a>
+				<a href="#Contact">Contact</a>
+			</strong>
+		</div>
+	</header>
+	<section id="Home" class="Home">
+			<h2>Hai Teman-Teman</h2>
+			<h2>Nama Saya Dhani Rizky</h2>	
+	<img src="/image/foto.jpeg" width="200px" height="200px">
+	</section>
+    <section id="Hoby" class="Hoby" style="background-image: url(img/bgmain.png)">
+        <div class="title">
+            <h2 class="section-title">Hoby</h2>
+            <div class="content">
+			<div class="komplit">
+				<div class="sepak">
+					<img src="/image/bolpak.jpg" width="120px" height="100px">
+                    <h2>Sepak Bola</h2>
+			</div>
+				<div class="Badminton">
+					<img src="/image/tonkok.jpg" width="120px" height="100px">
+                    <h2>Badminton</h2>
+			</div>
+				<div class="lari">
+					<img src="/image/joging.jpg" width="120px" height="100px">
+                    <h2>Lari/Jogging</h2>
+                </div>
+            </div>
+    </section>
+	<section class="About" id="About">
+		<div class="title">
+			<h2 class="section-title">About</h2>
+		</div>
+			<div class="col-left">
+				<div class="img-card">
+                    <center>
+					<img src="/image/foto 1.jpeg" width="200px" height="200px">
+                    <h2>Hai Teman-Teman,Perkenalkan Saya Dhany Rizky Finanto.Saat Ini Saya Menempuh Pendidikan Di Smk Telkom Purwokerto
+                     Domisil Saya Di Purbadana 04/01 Kec.Kembaran</h2>
+                </center>
+				</div>
+			</div>
+			<div class="card">
+				<div class="img-card">
+		</div>
+	</section>
+	<section id="Contact" class="Contact"  style="background-image: url(img/bgmain.png)">
+		<div class="title">
+			<h2 class="section-title">Contact</h2>
+		<div class="content">
+			<div class="card">
+				<div class="img-card"><a href="https://wa.me/08985195830" target="blank" />
+					<img src="/image/wabun.jpeg" /></a></div>
+                    <h2>Whatshap</h2>
+			</div>
+			<div class="card">
+				<div class="img-card"><a href="https://instagram.com/dancet__?igshid=NGVhN2U2NjQ0Yg==" target="blank" />
+					<img src="/image/igbun.jpeg" /></a></div>
+                    <h2>instagram</h2>
+			</div>
+            <div class="card">
+				<div class="img-card"><a href="https://www.facebook.com/profile.php?id=100087499370844&mibextid=9R9pXO" target="blank" />
+					<img src="/image/fbun.jpeg" /></a></div>
+                    <h2>facebook</h2>
+			</div>
+		</div>
+	</section>
+</body>
+</html>
